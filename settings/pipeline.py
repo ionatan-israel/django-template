72d83c9bc52dabe271561d103153c73b7a6b88ac
Jonatan Rodríguez <jrperdomoz@gmail.com>
2013-07-08 21:43:19 +0100

    agrego en settings pipeline.py

---
+++ settings/pipeline.py
@@ -0,0 +1,31 @@
+# encoding:utf-8
+##### PIPELINE ################################################################
+STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
+
+PIPELINE_CSS = {
+    'estilos': {
+        'source_filenames': (
+            'stylus/prueba.styl',
+        ),
+        'output_filename': 'css/s.css',
+        'extra_context': {
+            'media': 'screen,projection',
+        },
+    },
+}
+
+PIPELINE_JS = {}
+"""
+    'standard': {
+        'source_filenames': [
+            'js/sample1.coffee',
+        ],
+        'output_filename': 'js/s.js',
+    }
+"""
+
+
+PIPELINE_COMPILERS = (
+    'pipeline.compilers.stylus.StylusCompiler',
+    'pipeline.compilers.coffee.CoffeeScriptCompiler',
+)
