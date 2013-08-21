500c5fd587b60ddb3e6e756f6e353564b51aaca3
Jonatan Rodríguez <jrperdomoz@gmail.com>
2013-08-21 20:45:09 +0100

    update production

--- settings/production.py
+++ settings/production.py
@@ -21,7 +21,7 @@ DATABASES = {
 ########### AMAZON S3
 AWS_STORAGE_BUCKET_NAME = ''
 AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = 'jSaiHSM+'
+AWS_SECRET_ACCESS_KEY = ''
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
 STATIC_URL = S3_URL
